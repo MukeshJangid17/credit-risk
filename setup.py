9bbb3a3c02c2ec8335379fc605e7cd4f1c2548f1
MukeshJangid17 <mukeshjangid7877@gmail.com>
2024-09-12 12:04:51 +0530

    added files from cokkiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is a project in a finance domain of credit Risk modeling',
+    author='Mukesh Jangid',
+    license='',
+)
